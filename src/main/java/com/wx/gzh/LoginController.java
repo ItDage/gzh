5d2fd6715a5267a100f5e88dba1cd7a8c5f8804d
sunchuiyou <sunchuiyou@thunisoft.com>
2019-03-07 17:59:40 +0800

    首次提交

---
+++ src/main/java/com/wx/gzh/LoginController.java
@@ -0,0 +1,45 @@
+package com.wx.gzh;/**
+ * Created by Administrator on 2019/2/21 0021.
+ */
+
+import com.wx.util.CheckUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @ClassName LoginController
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2019/2/21 0021 下午 9:37
+ * @Version 1.0
+ **/
+@Controller
+public class LoginController {
+    @RequestMapping(value = "login",method= RequestMethod.GET)
+    public void login(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("success");
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            if(CheckUtil.checkSignature(signature, timestamp, nonce)){
+                out.write(echostr);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            out.close();
+        }
+
+    }
+}
